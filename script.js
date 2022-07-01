abf351bde3582e93e68028a1fbcd0a76c8f3bb6c
Igor <mr.igor187@gmail.com>
2022-07-01 07:46:41 +0200

    add styles and javascript for grid

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector('.container');
+let gridSize = 16; //16x16
 
-for(let i = 0; i < 16; i++)
+for(let i = 0; i < gridSize*gridSize; i++)
 {
     const grid = document.createElement('div');
     grid.classList.add('new-block');
     container.appendChild(grid);
 }
+
+grid.style.height = '92px';
+grid.style.width = '92px';
