bebf6342fea5f87497bee89a818f44e3fb9b4d13
Igor <mr.igor187@gmail.com>
2022-07-01 08:48:36 +0200

    add restrictions to gridSize value

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const container = document.querySelector('.container');
 const startButton = document.querySelector('button');
 
-let gridSize;
-
 startButton.addEventListener('click', () => {
-    gridSize = prompt("Enter grid size:");
+    let gridSize = prompt("Enter grid size:");
+
+    if(gridSize > 64) alert('Please enter values less than or equal to 64');
+    else
+    {
+        for(let i = 0; i < (gridSize*gridSize); i++)
+        {
+        const grid = document.createElement('div');
+        grid.classList.add('new-block');
+        container.appendChild(grid);
+        }
+    }
 });
-
-
-for(let i = 0; i < gridSize*gridSize; i++)
-{
-    const grid = document.createElement('div');
-    grid.classList.add('new-block');
-    container.appendChild(grid);
-}
 
 container.addEventListener('mouseover', (e) => {
     if(e.target == container) e.target.style.backgroundColor = 'white';
