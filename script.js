b33427de4edbfa82128fd02fd0cce3c37c97a919
Igor <mr.igor187@gmail.com>
2022-07-02 08:14:12 +0200

    add ability for user to select any number of rows and cols and make an numXnum grid

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const container = document.querySelector('.container');
 const startButton = document.querySelector('button');
 
 startButton.addEventListener('click', () => {
+    resetGrid();
+
     let gridSize = prompt("Enter grid size:");
+    //rows = gridSize, cols = gridSize
+
+    //createGrid(rows, cols);
 
     if(gridSize > 64) alert('Please enter values less than or equal to 64');
     else
-    {
+    {   
+        container.style.border = 'none';
+        container.style.setProperty('--gridRows', gridSize);
+        container.style.setProperty('--gridCols', gridSize);
+
         for(let i = 0; i < gridSize*gridSize; i++)
         {
             const grid = document.createElement('div');
-            grid.classList.add('new-block');
-            container.appendChild(grid);
+            container.appendChild(grid).classList.add('new-block');
         }
     }
 });
@@ -20,3 +28,18 @@ container.addEventListener('mouseover', (e) => {
     if(e.target == container) e.target.style.backgroundColor = 'white';
     else e.target.style.backgroundColor = 'black';
 });
+
+const resetGrid = () => container.innerHTML = '';
+
+function createGrid(gridSize)
+{
+    container.style.setProperty('--gridRows', gridSize);
+    container.style.setProperty('--gridCols', gridSize);
+
+    for(let i = 0; i < gridSize*gridSize; i++)
+    {
+        const grid = document.createElement('div');
+        container.appendChild(grid).classList.add('new-block');
+    }
+
+}
